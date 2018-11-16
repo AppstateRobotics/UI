aacea3b3b639cbafde314ae9b7111114eafd9f26
Dillon Carns <carnsds@appstate.edu>
2018-11-16 17:43:13 -0500

    Initial commit.

---
+++ Main.py
@@ -0,0 +1,16 @@
+'''
+Appstate Robotics
+@author Dillon Carns
+@author Eli McGalliard
+@author Jason Kennedy
+@author Nick Burkett
+'''
+
+
+def main():
+    print("Appstate Robotics")
+    #TODO: Do everyting ui related.
+
+
+if __name__ == "__main__":
+    main();
